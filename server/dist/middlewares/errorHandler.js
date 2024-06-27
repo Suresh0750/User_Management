ea7d7260ffd5a8be4d589a8f1352dfd10431f75d
Suresh0750 <sureshvarma0750@gmail.com>
2024-06-27 05:51:30 -0700

    Remove secrets folder from repository and add to .gitignore

--- server/dist/middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const errorHandler = (error, req, res, next) => {
-    console.log('errorHandler', error.message);
-    let errorMessage = error.message || "An unexpected error";
-    res.status(404).send(errorMessage);
-};
-exports.errorHandler = errorHandler;
