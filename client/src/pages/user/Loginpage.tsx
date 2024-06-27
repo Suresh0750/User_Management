5d7a33a1be8a4e1cf9b41c7c4afaeda6d409a9d1
Suresh0750 <sureshvarma0750@gmail.com>
2024-06-27 06:49:48 -0700

    login validation finish

--- client/src/pages/user/Loginpage.tsx
+++ client/src/pages/user/Loginpage.tsx
@@ -1,18 +1,73 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 
 import Button from '../../component/Button.jsx'
+import {useForm, SubmitHandler} from 'react-hook-form'
 import{Link} from 'react-router-dom'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+import Users from '../../Utils/UserDetailtypes.js'
+
+// * valdition schema
+const schema = yup.object().shape({
+  userEmail: yup.string().email('Please enter a valid email').required('Email is mandatory'),
+  passWord: yup.string()
+  .required('Password is mandatory')
+  .min(6, 'Minimum 6 characters required')
+  .max(15, 'Password should be less than 15 characters')
+  .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
+  .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
+  .matches(/[0-9]/, 'Password must contain at least one number')
+  .matches(/[@$!%*?&#]/, 'Password must contain at least one special character')
+})
+
+
 
 const Loginpage = () => {
+
+  
+  const {register,handleSubmit,formState:{errors}} = useForm({
+    resolver : yupResolver(schema)
+  })
+
+
+  // * use pick in login data 
+  type loginData = Pick<Users, "userEmail" | "passWord">
+
+  // * show the input error
+  const [error,setErrors] = useState<string>('')
+
+  // * show the error message to the user
+  useEffect(()=>{
+    for(const key in errors){
+      // console.log(`key`,errors[key]["message"])
+      // let msg = errors[key].message as string
+      if(errors[key]['message']){
+        setErrors(errors[key]["message"] as string)
+        break;
+      }
+    }
+    if(!Object.keys(errors)?.length){
+      setErrors('')
+    }
+  },[Object.keys(errors).length])
+
+
+  const onSubmit : SubmitHandler<loginData> = async data=>{
+
+  }
+
   return (
     <div className='grid grid-cols-1 lg:grid-cols-3 w-[80%] h-[60vh] box-border mx-[10%] my-[20vh] shadow-lg rounded-lg font-sans bg-white'>
       <div className='w-[100%] col-span-2  h-full grid place-items-center'>
           <div>
             <h2 className='text-5xl font-extrabold pb-3 text-slate-900 '>Login to Your Account </h2>
             <div className='grid '>
-                <input type="email" placeholder='Email' className='border text-black font-serif font-bold border-none  w-[98%] mb-[18px] p-2 rounded-[6px] bg-green-100 ' />
-                <input type="password" placeholder='Password' className='border text-black font-serif font-bold border-none  w-[98%] mb-[18px] p-2 rounded-[6px] bg-green-100 ' />
+            {error && <p className='text-red-600 font-bold'>{error}</p> }
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="email" placeholder='Email' {...register("userEmail",{ required:"Email is required"})} className='border text-black font-serif font-bold border-none  w-[98%] mb-[18px] p-2 rounded-[6px] bg-green-100 ' />
+                <input type="password" placeholder='Password' {...register("passWord",{ required:"Password is required"})} className='border text-black font-serif font-bold border-none  w-[98%] mb-[18px] p-2 rounded-[6px] bg-green-100 ' />
                 <Button>Sign In</Button>
+              </form>
             </div>
           </div>
       </div>
--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -2,6 +2,7 @@
 import { Request,Response,NextFunction } from 'express'
 import { signupValidator } from '../helpers/formValidation'
 import bcrypt from 'bcryptjs'
+import pool from '../config/dbConnect'
 
 
 export default {
@@ -11,12 +12,24 @@ export default {
            
             const validate = signupValidator(req.body)
             
-            console.log("validate",validate)
+            console.log(validate)
             if(!validate) throw new Error("Invalid Data")
-
             
-            console.log(req.body,'req body')
-            // res.send('developer')
+            const {userName,userEmail,passWord,mobileNo} = req.body
+            
+            // * for check or identify error came from database or not
+            try{
+            const encryptedPassword = await bcrypt.hash(passWord,10)
+            
+            const query = `INSERT INTO users(userName, email, phone, password) VALUES ($1,$2,$3,$4)`;
+
+            let storedata = await pool.query(query ,[userName,userEmail,mobileNo, encryptedPassword])
+            console.log(`storedate`,storedata)
+
+            }catch(error:any){
+                next(error)
+            }
+           
             res.status(200).send('back end working')
         }catch(error:any){
             next(error)
